aa315b5df9a0664a67be5b3670829a00d6ec004e
Tejas Shinde <52359615+tejasshinde2113@users.noreply.github.com>
2024-11-15 11:50:32 -0500

    ass 5 env

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -31,6 +31,7 @@ export default function UserRoutes(app) {
     const signin = (req, res) => { 
         const { username, password } = req.body;
         const currentUser = dao.findUserByCredentials(username, password);
+        console.log(currentUser);
         if (currentUser) {
             req.session["currentUser"] = currentUser;
             res.json(currentUser);
